e3a2d2061592d6deb09743e1bafbd98b109f5537
DzmitryKas <Kasperovich_dima@mail.ru>
2019-08-14 11:26:57 +0300

    feat: add notification

--- src/components/chat/Main.js
+++ src/components/chat/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import MessageList from "./messageList/messageList";
 import Header from "./header/header";
 import SendMessage from "./sendMessage/sendMessage";
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
 const url = "ws://st-chat.shas.tel";
 
 export default class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       messages: [],
       name: "try",
@@ -15,7 +17,9 @@ export default class App extends React.Component {
       connect: ""
     };
     this.sendMessage = this.sendMessage.bind(this);
-    // this.showMessage = this.showMessage.bind(this);
+    this.addNotification = this.addNotification.bind(this);
+    this.notificationDOMRef = React.createRef();
+    
   }
 
   ws = new WebSocket(url);
@@ -30,6 +34,8 @@ export default class App extends React.Component {
       // on receiving a message, add it to the list of messages
       const message = JSON.parse(evt.data);
       this.addMessage(message);
+      console.log(message);
+      this.addNotification(message[0].message);
     };
 
     this.ws.onclose = () => {
@@ -40,6 +46,20 @@ export default class App extends React.Component {
         ws: new WebSocket(url)
       });
     };
+  }
+
+  addNotification(msg) {
+    this.notificationDOMRef.current.addNotification({
+      title: "message",
+      message: msg,
+      type: "success",
+      insert: "bottom",
+      container: "bottom-right",
+      animationIn: ["animated", "fadeIn"],
+      animationOut: ["animated", "fadeOut"],
+      dismiss: { duration: 2000 },
+      dismissable: { click: true }
+    });
   }
 
   addMessage = message =>
@@ -54,7 +74,7 @@ export default class App extends React.Component {
       const message = JSON.parse(evt.data);
       this.addMessage(message);
     };
-  };
+  }; 
 
   render() {
     return (
@@ -62,6 +82,7 @@ export default class App extends React.Component {
         <Header connect={this.state.connect}/>
         <MessageList messages={this.state.messages} name={this.state.name}/>
         <SendMessage sendMessage={this.sendMessage} />
+        <ReactNotification ref={this.notificationDOMRef} />
       </div>
     );
   }
